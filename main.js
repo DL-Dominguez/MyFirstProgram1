24a8f9c22f55b72bd4463514cd4483d8ac6a7352
DL-Dominguez <Dominguez@mail.fresnostate.edu>
2018-09-11 20:33:24 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+var MyFirstProgram1 = /** @class */ (function () {
+    function MyFirstProgram1() {
+        var argArray = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            argArray[_i] = arguments[_i];
+        }
+    }
+    MyFirstProgram1.prototype.PrintArgs = function (argArray) {
+        console.log(argArray);
+    };
+    MyFirstProgram1.prototype.runFirst = function (callback) {
+        console.log('this method runs first');
+    };
+    MyFirstProgram1.prototype.runLast = function () {
+        console.log('this method runs last');
+    };
+    return MyFirstProgram1;
+}());
+var newInstance = new MyfirstProgram1("a", "b", "c");
---
+++ main.ts
@@ -0,0 +1,17 @@
+class MyFirstProgram1 {
+
+    constructor(...argArray: Array<any>) {
+
+    }
+    PrintArgs(argArray) {
+        console.log(argArray);
+    }
+    runFirst(callback: Function) {
+        console.log('this method runs first');
+    }
+    runLast() {
+        console.log('this method runs last');
+    }
+}
+const newInstance = new MyfirstProgram1("a","b","c");
+
